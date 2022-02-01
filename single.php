85ca199f92ca740aa009b4381e5b4523fbbbc69f
Rasmus Bergström <58551229+rasmusbergstrom@users.noreply.github.com>
2022-02-01 09:10:41 +0100

    Update single.php
    Create loop for  category, fetch title and excerpt from post

--- single.php
+++ single.php
@@ -1,36 +1,49 @@
 <?php get_header(); ?>
-    <section class="start-section">
-        <div class="single-portfolio-card">
-                <div class="portfolio-header">
-                    <div class="portfolio-label">
-                        <h4>Portfolio 1</h4>
-                    </div>
-                </div>
-                <article class="portfolio-content">
-                    <div class="content-text">
-                        <h4>Portfolio del 1</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui distinctio ducimus nobis, mollitia nam enim saepe vitae, sunt optio magni beatae. Vero aspernatur est obcaecati tenetur expedita minima doloribus veniam!</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui distinctio ducimus nobis, mollitia nam enim saepe vitae, sunt optio magni beatae. Vero aspernatur est obcaecati tenetur expedita minima doloribus veniam!</p>
-                    </div>                   
-                    <div class="portfolio-img-container">
-                        <figure>
-                            <img src="img/example3.mobile.png" alt="">
-                        </figure>
-                        <figure>
-                            <img src="img/example1.mobile.png" alt="">
-                        </figure>
-                    </div>
-                </article>
-                <div class="portfolio-category-div">
-                    <div class="category"></div>
-                </div>
+<section class="start-section">
+<?php 
+    if(have_posts()) : 
+        while(have_posts()) :
+            the_post();
+?>  
+    <div class="single-portfolio-card">
+        <div class="portfolio-header">
+            <div class="portfolio-label">
+                <h4><?php the_title(); ?></h4>
             </div>
-    </section>
-    <footer>
-        <div class="footer-container">
-            <h5>Rasmus Bergström 2022</h5>
         </div>
-    </footer>
-    <script src="script.js"></script>
+        <article class="portfolio-content">
+            <div class="content-text">
+                <h4><?php the_title(); ?></h4>
+            <p><?php the_excerpt(); ?></p>
+            </div>
+            <div class="portfolio-category-div">
+                <ul class="categories"> 
+                <?php $categories = get_the_category();
+                    foreach ($categories as $category) {
+                       $catName = $category -> name;
+                       $catLink = get_category_link($category -> term_id);
+                       echo "<li><a href='$catLink'>". $catName ."</a></li>";
+                    }
+                ?>
+                </ul>
+            </div>                   
+            <div class="portfolio-img-container">
+                <figure>
+                    <img src="img/example3.mobile.png" alt="">
+                </figure>
+                <figure>
+                    <img src="img/example1.mobile.png" alt="">
+                </figure>
+            </div>
+        </article>
+    </div>
+<?php 
+        endwhile;
+    else : ?>
+        <p><?php esc_html_e('Det finns inget innehåll')?></p>
+<?php endif;
+?>
+</section>
+<?php get_footer(); ?>
 </body>
 </html>
