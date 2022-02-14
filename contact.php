99be1a9b334a667d1751e292352d60a50fbea8b5
Rasmus Bergström <58551229+rasmusbergstrom@users.noreply.github.com>
2022-02-14 12:18:12 +0100

    Clean up style.scss
    Move some styling to another section, move back button to middle in contact-form.

--- contact.php
+++ contact.php
@@ -8,16 +8,13 @@ if(have_posts()) :
         the_post();
 ?>
 <section class="singlecontainer-section">
-<?php 
-    
-?>
 <article class="card-content">
     <div class="form-div">
         <h1> <?php the_title(); ?></h1>
         <?php echo apply_shortcodes( '[contact-form-7 id="187" title="Kontaktformulär 1" html_id="contact-form"]' ); ?>
     </div>
 </article>
-<div class="btn-bar end">
+<div class="btn-bar single">
         <div class="btn">
             <a href="javascript:history.back()">
                 Tillbaka
