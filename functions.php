2d4cfb5b6c8a7242c4586e5a4cb0666252ff7258
Rasmus Bergström <58551229+rasmusbergstrom@users.noreply.github.com>
2022-02-14 18:02:19 +0100

    Change excerpt to 35 chars

--- functions.php
+++ functions.php
@@ -17,15 +17,21 @@ function theme_setup() {
     ));
 }
 add_action('after_setup_theme', 'theme_setup');
-
+//Lägga till egen klass på a tag för styling
 function add_class_for_previous_post($html){
     $html = str_replace('<a', '<a class="btn"', $html);
     return $html;
 }
 add_filter('previous_post_link', 'add_class_for_previous_post');
-
+//Lägga till egen klass på a tag för styling
 function add_class_for_next_post($html){
     $html = str_replace('<a', '<a class="btn"', $html);
     return $html;
 }
 add_filter('next_post_link', 'add_class_for_next_post');
+
+//Ändra antalet tecken i excerp
+function my_excerpt_length($length){
+    return 35;
+}
+add_filter('excerpt_length', 'my_excerpt_length');
